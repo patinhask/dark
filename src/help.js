0426ed739d92c64993e96030e8ffc59c018f0b00
patinhask <78981986+patinhask@users.noreply.github.com>
2021-02-14 19:26:57 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,84 @@
+const //database
+	return `
+╔══✪〘 Informações 〙✪══
+║
+║───────⊹⊱✫⊰⊹───────
+║➩ ❍ Criador: ${botName}
+║➩ ❍ wa.me/5511934660977
+║➩ ❍ Prefix: 「  ${prefix}  」
+║➩ ❍ Nome: ${pushname}️
+───────⊹⊱✫⊰⊹───────
+
+❍ 𝚂𝙾𝙱𝚁𝙴 
+  ║➩ ❍ ${prefix}ping
+
+❍ 𝙵𝙰𝚉𝙴𝚁
+  ║➩ ❍ *${prefix}figu*
+  ║➩ ❍ *${prefix}toimg* (Transformar fig em foto)
+  ║➩ ❍ ${prefix}tomp3 (Transformar video em aúdio)
+  ║➩ ❍ ${prefix}ttp (texto)
+  ║➩ ❍ ${prefix}tts
+
+❍ 𝙵𝙰𝙻𝙰𝚁 𝙲𝙾𝙼 𝙱𝙾𝚃 
+║➩ ❍ ${prefix}simi (sua mensagem)
+
+❍ 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳
+
+  ║➩ ❍ ${prefix}play (nome da msc)
+  ║➩ ❍ ${prefix}ytmp4
+
+❍ 𝙰𝙽𝙸𝙼𝙴𝚂
+  
+ ║➩ ❍ ${prefix}loli  (off)
+ ║➩ ❍ ${prefix}loli1
+ ║➩ ❍ ${prefix}hentai
+ ║➩ ❍ ${prefix}animehug
+
+❍ 𝙾𝚄𝚃𝚁𝙾𝚂
+ ║➩ ❍ ${prefix}dado
+ ║➩ ❍ ${prefix}cekvip
+ ║➩ ❍ ${prefix}premiumlist
+ ║➩ ❍ ${prefix}delete
+ ║➩ ❍ ${prefix}daftarvip [para virar Premium]
+ ║➩ ❍ ${prefix}qrcode
+ ║➩ ❍ ${prefix}gcpf
+ ║➩ ❍ ${prefix}pinterest (nome)
+ ║➩ ❍ ${prefix}gay @
+ ║➩ ❍ ${prefix}porno
+  ❍𝙼𝙴𝙽𝚄 𝙰𝙳𝙼𝙸𝙽
+  
+ ║➩ ❍ ${prefix}abrirgp
+ ║➩ ❍ ${prefix}fechargp
+ ║➩ ❍ ${prefix}promover (Tornar membro comum em adm)
+ ║➩ ❍ ${prefix}rebaixar(Tornar adm em membro comum)
+ ║➩ ❍ ${prefix}marcar
+ ║➩ ❍ ${prefix}marcar2
+ ║➩ ❍ ${prefix}marcar3
+ ║➩ ❍ ${prefix}add ex: add 55....
+ ║➩ ❍ ${prefix}banir @tag membro
+ ║➩ ❍ ${prefix}listadmins
+ ║➩ ❍ ${prefix}linkgp
+ ║➩ ❍ ${prefix}antilinkgroup <1/0>
+ ║➩ ❍ ${prefix}bemvindo [1/0]
+ ║➩ ❍ ${prefix}nsfw
+ ║➩ ❍ ${prefix}leveling [on/off]
+ ║➩ ❍ ${prefix}level
+ ║➩ ❍ ${prefix}delete (Apagar msg q o bot mandou)
+ ║➩ ❍ ${prefix}simih 1/0
+
+❍ 𝙳𝙾𝙽𝙾
+
+  ║➩ ❍ ${prefix}setprefix
+  ║➩ ❍ ${prefix}exe
+  ║➩ ❍ ${prefix}tm
+  ║➩ ❍ ${prefix}tmctt
+  ║➩ ❍ ${prefix}clonar
+  ║➩ ❍ ${prefix}clearall
+  ║➩ ❍ ${prefix}desligar
+  ║➩ ❍ ${prefix}timer
+  ║➩ ❍ ${prefix}bloquear @
+  ║➩ ❍ ${prefix}desbloquear @
+}
+
+//Online.database
+
